2241181fa64f3ea9ccb9c7e1fac75ed2e89da4c9
Bram Borggreve <borggreve@gmail.com>
2017-06-15 03:35:57 -0500

    refactor: move code from plopfile.js in separate modules

---
+++ templates/api-module.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk')
+const log = require('../lib/logger')
+
+const description = 'Generate a LoopBack module for @colmena/api'
+
+const prompts = []
+
+const actions = (data) => {
+  const actions = []
+
+  return actions
+}
+
+module.exports = {description, prompts, actions}
